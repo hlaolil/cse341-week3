cef59f681bcff80077eb1cd9cfd85819c7598753
hlaolil <hla24002@byui.edu>
2025-10-03 11:59:25 +0200

    week 3

--- data/database.js
+++ data/database.js
@@ -17,7 +17,7 @@ async function mongoDB() {
   if (!db || !client.topology.isConnected()) {
     try {
       await client.connect();
-      db = client.db('grace-pharmacy');
+      db = client.db('tlokza_db_user');
       console.log('✅ Connected to MongoDB');
     } catch (err) {
       console.error('Error connecting to MongoDB:', err.message);
